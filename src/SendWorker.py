4a068054dfcbecd27f9f1a4a114791acb25b1881
chernykh_vv <chernykh_vv@N12663.corp.nomos.bank>
2017-01-23 11:00:11 +0300

    init

--- src/SendWorker.py
+++ src/SendWorker.py
@@ -1,3 +1,6 @@
+#!/usr/bin/env python
+# -*- coding: UTF-8 -*-
+#
 # To change this license header, choose License Headers in Project Properties.
 # To change this template file, choose Tools | Templates
 # and open the template in the editor.
@@ -5,27 +8,27 @@
 __author__ = "chernykh_vv"
 __date__ = "$20.01.2017 18:03:06$"
 
-#!/usr/bin/env python
-# -*- coding: UTF-8 -*-
-#
-
 import threading
 from datetime import datetime
 
 # send thread
 class SendThread(threading.Thread):
 
+    def setCount(self, count):
+        self.count = count
+        return self
+
     def run(self):
-        count = 10
         # send some messages
         threadName = self.getName()
         print ('%s start working' % threadName)
-        for i in range(count):
-           msg = datetime.now().strftime('%Y-%m-%d %H:%M:%S') + ' message:' + str(i) 
+        for i in range(self.count):
+           msg = datetime.now().strftime('%Y-%m-%d %H:%M:%S,%f') + ' ' + threadName + ' message:' + str(i) 
            try:
                print ('%s: send succesful: %s' % (threadName,msg))
            except:
                print ('%s: failed to send: %s' % (threadName,msg))
         print ('%s stop working' % threadName)
+        return self
 
 
--- src/Spamthreads.py
+++ src/Spamthreads.py
@@ -1,3 +1,6 @@
+#!/usr/bin/env python
+# -*- coding: UTF-8 -*-
+#
 # To change this license header, choose License Headers in Project Properties.
 # To change this template file, choose Tools | Templates
 # and open the template in the editor.
@@ -5,12 +8,16 @@
 __author__ = "chernykh_vv"
 __date__ = "$20.01.2017 18:04:06$"
 
-#!/usr/bin/env python
-
 import threading
 
 from SendWorker import SendThread
 
-# start two send threads
-for x in range(2):
-    SendThread().start()
+max_th_val = 3
+max_msg_count = 5
+
+
+if __name__ =='__main__':		# multy processing template
+    for x in range(max_th_val):		# start two send threads
+        th_obj = SendThread().setCount(max_msg_count)
+        th_obj.start()
+
